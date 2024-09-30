4589cf471b9692a8f7d83811ae6b382f399a1877
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-09-30 16:12:17 -0300

    config conexao e close conexao

---
+++ src/config/database.ts
@@ -0,0 +1,19 @@
+import sqlite3  from "sqlite3";
+
+export const db = new sqlite3.Database('library_db', (err: Error | null)=>{
+    if(err){
+        console.error(`Erro ao conectar no banco de dados: ${err.message}`)
+    }else{
+        console.log('Conectado com sucesso ao banco de dados Sqlite')
+    }
+});
+
+export function closeDb(){
+    db.close((err:Error | null)=>{
+        if(err){
+            console.error(`Erro ao fechar o banco de dados: ${err.message}`)
+        }else{
+            console.log('Conexão fechada com sucesso.')
+        }
+    })
+}
