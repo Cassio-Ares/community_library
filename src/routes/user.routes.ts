4fe9652dd2026a1e7b7d300ccfadaf33507a34eb
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-10-01 16:11:31 -0300

    validate zod

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import userController from "../controller/user.controller";
 
+//validate and middlewares
+import { validate } from "../middlewares/validation.middleware";
+import { userSchema } from "../schema/user.schema";
+
+
+
 const routerUsers = Router()
 
-routerUsers.post('/users', userController.createUserController)
+routerUsers.post('/users',validate(userSchema), userController.createUserController)
 
 export default routerUsers
---
+++ src/schema/user.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+   username: z.string().min(3, 'Username is required'),
+   email: z.string().email("Invalid email"),
+   password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character"),
+   avatar: z.string().optional(), // Defina um campo opcional
+});
