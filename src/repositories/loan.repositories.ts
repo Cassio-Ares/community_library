2e0c7107d8a23c10298346ca83a70c33b3ba1dc6
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-10-18 16:23:04 -0300

    join in slq

--- src/repositories/loan.repositories.ts
+++ src/repositories/loan.repositories.ts
@@ -39,20 +39,61 @@ function createLoanRepository(userId:number, bookId:number, dueDate:Date):Promis
   })
 }
 
-function findAllLoansRepository():Promise<Loans[] | []> {
+// function findAllLoansRepository():Promise<Loans[] | []> {
+//     return new Promise((res, rej)=>{
+//         db.all(`
+//             SELECT *
+//             FROM loans
+//         `, [], (err:any, rows:Loans[])=>{
+//             if(err){
+//                 rej(err)
+//             }else{
+//                 res(rows || [])
+//             }
+//         })
+//     })
+// }
+
+
+/**
+ * Esta query retorna os detalhes dos empréstimos de livros,
+ * incluindo o ID do empréstimo, a data de vencimento, o e-mail do usuário
+ * que realizou o empréstimo e o título do livro emprestado.
+ * Ela realiza junções entre as tabelas de empréstimos, usuários e livros para obter essas informações.
+ * 
+ * explicação do join:
+ *      JOIN user ON loans.userId = users.id 
+ *      Junte a tabela users com a tabela loans onde o campo userId da tabela loans for igual ao campo id da tabela users.
+ * 
+ * 
+ *  [
+ *    {
+ *      "id": 2,
+ *      "dueDate": "2024-10-19",
+ *      "email": "Te@Teste.com",
+ *      "title": "A arte da Guerra"
+ *     }
+ *  ]
+ */
+
+function findAllLoansRepository():Promise<Loans[] | []>{
     return new Promise((res, rej)=>{
-        db.all(`
-            SELECT *
-            FROM loans
-        `, [], (err:any, rows:Loans[])=>{
-            if(err){
-                rej(err)
+         db.all(`
+         SELECT loans.id, loans.dueDate, users.email, books.title
+         FROM loans
+         JOIN users ON loans.userId = users.id   
+         JOIN books ON loans.bookId = books.id
+        `, [], (error:any, rows: Loans[])=>{
+            if(error){
+                rej(error)
             }else{
                 res(rows || [])
             }
         })
-    })
+    }) 
 }
+
+
 
 function findLoanByIdRepository(loanId:number):Promise<Loans | null>{
     return new Promise((res, rej)=>{
